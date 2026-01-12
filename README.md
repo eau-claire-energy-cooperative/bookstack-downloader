6b90ff228b2b383940f3d1c45a2af58dc959e560
Rob Weber <rweber@ecec.com>
2026-01-12 09:43:38 -0600

    connect to bookstack API and print system information

--- README.md
+++ README.md
@@ -5,7 +5,12 @@ Quickly export books, chapters, or pages from a [BookStack](https://www.bookstac
 
 ## Install
 
+Cloning and installing this repository will enable access to the `bookstack-downloader` tool via the command line. It is recommended to run this in a Python virtual environment.
+
 ```
+
+# clone the repository
+git clone https://github.com/eau-claire-energy-cooperative/bookstack-downloader
 
 # create the virtual environment
 python3 -m venv .venv
@@ -13,7 +18,8 @@ python3 -m venv .venv
 # activate and install dependencies
 source .venv/bin/activate
 
-# install the requirements
+# install the downloader
+pip3 install .
 
 ```
 
--- src/bookstack_downloader/downloader.py
+++ src/bookstack_downloader/downloader.py
@@ -1,7 +1,21 @@
+import configargparse
+import bookstack
 
 def main():
-    print("running")
+    parser = configargparse.ArgumentParser(description='BookStack Downloader')
+    parser.add_argument('-c', '--config', is_config_file=True, help='Path to custom config file')
+    parser.add_argument('-u', '--url', required=True, help='base URL of the BookStack instance')
+    parser.add_argument('-T', '--token', required=True, help='authorization token')
+    parser.add_argument('-S', '--secret', required=True, help='authorization secret')
+    args = parser.parse_args()
 
+    # try and connect to the api
+    api = bookstack.BookStack(args.url, token_id=args.token, token_secret=args.secret)
+
+    # need to generate these or it won't work
+    api.generate_api_methods()
+
+    print(api.get_system_read())
 
 if __name__ == '__main__':
     main()
