47fbd04723b327484a599f5f60861fdfba72530b
Rob Weber <rweber@ecec.com>
2026-01-12 14:25:15 -0600

    can download all books in a shelf by book or by chapter/page

--- src/bookstack_downloader/downloader.py
+++ src/bookstack_downloader/downloader.py
@@ -1,21 +1,110 @@
-import configargparse
 import bookstack
+import configargparse
+import os
+import os.path
+
+class Downloader:
+    api = None
+    download_dir = "downloads"
+
+    def __init__(self, url, token, secret, download_dir):
+        self.api = bookstack.BookStack(url, token_id=token, token_secret=secret)
+        self.download_dir = download_dir
+
+        # generate these at the start or it won't work
+        self.api.generate_api_methods()
+
+    def _write_file(self, pdf_contents, path):
+        with open(path, "wb") as binary_file:
+            binary_file.write(pdf_contents)
+
+    def load_shelves(self):
+        result = {}
+
+        shelves = self.api.get_shelves_list()
+
+        # get name/id pairs
+        for s in shelves['data']:
+            result[s['slug']] = s['id']
+
+        return result
+
+    def export_book(self, book_id, split_book):
+
+        # load detailed information on this book
+        book_info = self.api.get_books_read({'id': book_id})
+        print(f"Extracting {book_info['name']}")
+
+        if(not split_book):
+            # download the entire book
+            pdf = self.api.get_books_export_pdf({"id": book_id})
+            self._write_file(pdf, f"{os.path.join(self.download_dir, book_info['name'])}.pdf")
+
+        else:
+            # create a download path specific to this book
+            path = create_folder(f"{self.download_dir}/{book_info['name']}")
+
+            # for each chapter/page in the book, download it
+            for item in book_info['contents']:
+                print(f"Saving {item['name']}: {item['type']}")
+                if(item['type'] == 'chapter'):
+                    pdf = self.api.get_chapters_export_pdf({"id": item['id']})
+                elif(item['type'] == 'page'):
+                    pdf = self.api.get_pages_export_pdf({"id": item['id']})
+                self._write_file(pdf, f"{os.path.join(path, item['name'])}.pdf")
+
+    def get_shelf(self, shelf_name):
+        result = None
+        shelves = self.load_shelves()
+
+        if(shelf_name in shelves):
+            result = self.api.get_shelves_read({"id": shelves[shelf_name]})
+        else:
+            print(f"{shelf_name} is not a valid shelf")
+
+        return result
+
+    def print_system_info(self):
+        info = self.api.get_system_read()
+
+        print(f"Connected to {info['app_name']} version {info['version']}")
+
+def create_folder(path):
+    if(not os.path.exists(path)):
+        os.makedirs(path)
+    return path
 
 def main():
     parser = configargparse.ArgumentParser(description='BookStack Downloader')
     parser.add_argument('-c', '--config', is_config_file=True, help='Path to custom config file')
-    parser.add_argument('-u', '--url', required=True, help='base URL of the BookStack instance')
-    parser.add_argument('-T', '--token', required=True, help='authorization token')
-    parser.add_argument('-S', '--secret', required=True, help='authorization secret')
+
+    auth_group = parser.add_argument_group("Authentication")
+    auth_group.add_argument('-u', '--url', required=True, help='base URL of the BookStack instance')
+    auth_group.add_argument('-T', '--token', required=True, help='authorization token')
+    auth_group.add_argument('-S', '--secret', required=True, help='authorization secret')
+
+    config_group = parser.add_argument_group("Download Settings")
+    config_group.add_argument('-d', '--directory', required=False, default="downloads", help="directory to download PDFs")
+    config_group.add_argument('-s', '--shelf', required=True, help="The slugified version of the shelf to export")
+    config_group.add_argument('-b', '--split-book', action='store_true', help='Split the book into separate PDFs instead of one big file')
     args = parser.parse_args()
 
     # try and connect to the api
-    api = bookstack.BookStack(args.url, token_id=args.token, token_secret=args.secret)
+    downloader = Downloader(args.url, args.token, args.secret, args.directory)
+    downloader.print_system_info()
 
-    # need to generate these or it won't work
-    api.generate_api_methods()
+    # try and find the shelf
+    shelf = downloader.get_shelf(args.shelf)
 
-    print(api.get_system_read())
+    if(shelf is not None):
+        if(len(shelf['books']) > 0):
+            print(f"{shelf['name']} has {len(shelf['books'])} books")
+
+            for b in shelf['books']:
+                downloader.export_book(b['id'], args.split_book)
+        else:
+            print(f"{shelf['name']} does not have books to export")
+
 
 if __name__ == '__main__':
     main()
